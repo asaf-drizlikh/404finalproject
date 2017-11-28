6ea2dd24de6a28db630cedbd878180bd6b68739e
Steve Broll <stevebroll@tamu.edu>
2017-11-27 18:17:15 -0600

    Add files via upload

---
+++ load_data.R
@@ -0,0 +1,4 @@
+links = read.csv('https://raw.githubusercontent.com/kateto/R-igraph-Network-Workshop-NetSciX/master/Dataset1-Media-Example-EDGES.csv')
+nodes = read.csv('https://github.com/kateto/R-igraph-Network-Workshop-NetSciX/blob/master/Dataset1-Media-Example-NODES.csv')
+links2 = read.csv('https://github.com/kateto/R-igraph-Network-Workshop-NetSciX/blob/master/Dataset2-Media-User-Example-EDGES.csv')
+nodes2 = read.csv('https://github.com/kateto/R-igraph-Network-Workshop-NetSciX/blob/master/Dataset2-Media-User-Example-NODES.csv')
